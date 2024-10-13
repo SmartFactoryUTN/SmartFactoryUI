2f0c3a8cb9b1a90f18c55352ead10f2bb8ba1230
Milena <milenalebedin@gmail.com>
2024-10-13 19:14:42 -0300

    updated base backend url. added test user id

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,1 +1,2 @@
-export const BASE_API_URL = 'http://ec2-54-207-255-148.sa-east-1.compute.amazonaws.com:8080/api';
+export const BASE_API_URL = 'https://smartfactoryapi.sa-east-1.elasticbeanstalk.com/api';
+export const TEST_USER_ID = '14bd6578-0436-420d-9c64-2beda866fcf0';
