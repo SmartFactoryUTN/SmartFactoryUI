719d63e221cbba7ff316bf1e26c2925b94ffa76f
Gastón Vidal <gnvidal25@gmail.com>
2024-10-21 22:06:46 -0300

    SF-117: hardcoded constants to be fixed

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,7 +1,7 @@
-export const REACT_APP_API_SERVER_URL: string = process.env.REACT_APP_API_SERVER_URL || 'http://localhost:8080/api'
-export const REACT_APP_AUTH0_DOMAIN: string = process.env.REACT_APP_AUTH0_DOMAIN || 'https://dummy.com'
-export const REACT_APP_AUTH0_CLIENT_ID: string = process.env.REACT_APP_AUTH0_CLIENT_ID || 'dummy'
-export const REACT_APP_AUTH0_CALLBACK_URL: string = process.env.REACT_APP_AUTH0_CALLBACK_URL || 'http://dummy.com'
+export const REACT_APP_API_SERVER_URL: string = process.env.REACT_APP_API_SERVER_URL || 'https://smartfactoryapi.sa-east-1.elasticbeanstalk.com/api'
+export const REACT_APP_AUTH0_DOMAIN: string = process.env.REACT_APP_AUTH0_DOMAIN || 'https://dev-palh3gsscosfpppk.us.auth0.com/'
+export const REACT_APP_AUTH0_CLIENT_ID: string = process.env.REACT_APP_AUTH0_CLIENT_ID || 'u9EbOfCWWYTDarJN8VxblpBFabtANScD'
+export const REACT_APP_AUTH0_CALLBACK_URL: string = process.env.REACT_APP_AUTH0_CALLBACK_URL || 'https://smartfactoryar.netlify.app/tizadas'
 export const REACT_APP_AUTH0_AUDIENCE: string= process.env.REACT_APP_AUTH0_AUDIENCE || 'https://smartfactory-api/'
 export const REACT_APP_AUTH0_SCOPES: string= process.env.REACT_APP_AUTH0_SCOPES || 'read:tizada read:molde create:tizada create:molde'
 export const BASE_API_URL: string = process.env.BASE_API_URL || REACT_APP_API_SERVER_URL;
