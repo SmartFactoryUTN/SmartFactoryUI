a1c01d62bab2ca0d828c82a2527c7d2fb4954982
Gastón Vidal <gnvidal25@gmail.com>
2024-10-21 21:45:26 -0300

    SF-117: env vars

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,8 +1,8 @@
-export const REACT_APP_API_SERVER_URL = 'http://localhost:8080/api'
-export const REACT_APP_AUTH0_DOMAIN = 'https://dummy.com'
-export const REACT_APP_AUTH0_CLIENT_ID = 'dummy'
-export const REACT_APP_AUTH0_CALLBACK_URL = 'http://dummy.com'
-export const REACT_APP_AUTH0_AUDIENCE= 'https://smartfactory-api/'
-export const REACT_APP_AUTH0_SCOPES= 'dummy'
-export const BASE_API_URL = REACT_APP_API_SERVER_URL;
-export const TEST_USER_ID = "dummy"
+export const REACT_APP_API_SERVER_URL = process.env.REACT_APP_API_SERVER_URL || 'http://localhost:8080/api'
+export const REACT_APP_AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || 'https://dummy.com'
+export const REACT_APP_AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || 'dummy'
+export const REACT_APP_AUTH0_CALLBACK_URL = process.env.REACT_APP_AUTH0_CALLBACK_URL || 'http://dummy.com'
+export const REACT_APP_AUTH0_AUDIENCE= process.env.REACT_APP_AUTH0_AUDIENCE || 'https://smartfactory-api/'
+export const REACT_APP_AUTH0_SCOPES= process.env.REACT_APP_AUTH0_SCOPES || 'read:tizada read:molde create:tizada create:molde'
+export const BASE_API_URL = process.env.BASE_API_URL || REACT_APP_API_SERVER_URL;
+export const TEST_USER_ID = '';
