7ade19dea6c37f1175886077b57c08829a1e695f
Gastón Vidal <gnvidal25@gmail.com>
2024-10-21 21:36:42 -0300

    SF-117: fix build

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -5,3 +5,4 @@ export const REACT_APP_AUTH0_CALLBACK_URL = 'http://dummy.com'
 export const REACT_APP_AUTH0_AUDIENCE= 'https://smartfactory-api/'
 export const REACT_APP_AUTH0_SCOPES= 'dummy'
 export const BASE_API_URL = REACT_APP_API_SERVER_URL;
+export const TEST_USER_ID = "dummy"
