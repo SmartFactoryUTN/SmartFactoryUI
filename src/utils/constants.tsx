b2bb1a41cd797ccc615b6faab4141795070ee712
Gastón Vidal <gnvidal25@gmail.com>
2024-10-22 22:47:21 -0300

    Fix envs

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,8 +1,8 @@
-export const REACT_APP_API_SERVER_URL: string = process.env.REACT_APP_API_SERVER_URL || 'https://smartfactoryapi.sa-east-1.elasticbeanstalk.com/api'
-export const REACT_APP_AUTH0_DOMAIN: string = process.env.REACT_APP_AUTH0_DOMAIN || 'https://dev-palh3gsscosfpppk.us.auth0.com'
-export const REACT_APP_AUTH0_CLIENT_ID: string = process.env.REACT_APP_AUTH0_CLIENT_ID || 'u9EbOfCWWYTDarJN8VxblpBFabtANScD'
-export const REACT_APP_AUTH0_CALLBACK_URL: string = process.env.REACT_APP_AUTH0_CALLBACK_URL || 'https://smartfactoryar.netlify.app/tizadas'
-export const REACT_APP_AUTH0_AUDIENCE: string= process.env.REACT_APP_AUTH0_AUDIENCE || 'https://smartfactory-api/'
-export const REACT_APP_AUTH0_SCOPES: string= process.env.REACT_APP_AUTH0_SCOPES || 'read:tizada read:molde create:tizada create:molde'
-export const BASE_API_URL: string = process.env.BASE_API_URL || REACT_APP_API_SERVER_URL;
+export const REACT_APP_API_SERVER_URL: string = import.meta.env.VITE_API_SERVER_URL || 'https://localhost:8080/api'
+export const REACT_APP_AUTH0_DOMAIN: string = import.meta.env.VITE_AUTH0_DOMAIN || 'https://dev-palh3gsscosfpppk.us.auth0.com'
+export const REACT_APP_AUTH0_CLIENT_ID: string = import.meta.env.VITE_AUTH0_CLIENT_ID || 'u9EbOfCWWYTDarJN8VxblpBFabtANScD'
+export const REACT_APP_AUTH0_CALLBACK_URL: string = import.meta.env.VITE_AUTH0_CALLBACK_URL || 'http://localhost:5173/tizadas'
+export const REACT_APP_AUTH0_AUDIENCE: string= import.meta.env.VITE_AUTH0_AUDIENCE || 'https://smartfactory-api/'
+export const REACT_APP_AUTH0_SCOPES: string= import.meta.env.VITE_AUTH0_SCOPES || 'read:tizada read:molde create:tizada create:molde'
+export const BASE_API_URL: string = import.meta.env.BASE_API_URL || REACT_APP_API_SERVER_URL;
 export const TEST_USER_ID = '14bd6578-0436-420d-9c64-2beda866fcf0';
