7e2d6e094e064cd9e9dd87169b44eef6df7a58b7
Gastón Vidal <gnvidal25@gmail.com>
2024-10-23 17:31:35 -0300

    Fix local constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,7 +1,7 @@
-export const REACT_APP_API_SERVER_URL: string = import.meta.env.VITE_API_SERVER_URL || 'https://localhost:8080/api'
+export const REACT_APP_API_SERVER_URL: string = import.meta.env.VITE_API_SERVER_URL || 'http://localhost:8080/api'
 export const REACT_APP_AUTH0_DOMAIN: string = import.meta.env.VITE_AUTH0_DOMAIN || 'https://dev-palh3gsscosfpppk.us.auth0.com'
 export const REACT_APP_AUTH0_CLIENT_ID: string = import.meta.env.VITE_AUTH0_CLIENT_ID || 'u9EbOfCWWYTDarJN8VxblpBFabtANScD'
-export const REACT_APP_AUTH0_CALLBACK_URL: string = import.meta.env.VITE_AUTH0_CALLBACK_URL || 'https://smartfactoryar.netlify.app/callback'
+export const REACT_APP_AUTH0_CALLBACK_URL: string = import.meta.env.VITE_AUTH0_CALLBACK_URL || 'http://localhost:5173/callback'
 export const REACT_APP_AUTH0_AUDIENCE: string= import.meta.env.VITE_AUTH0_AUDIENCE || 'https://smartfactory-api/'
 export const REACT_APP_AUTH0_SCOPES: string= import.meta.env.VITE_AUTH0_SCOPES || 'read:tizada read:molde create:tizada create:molde'
 export const BASE_API_URL: string = import.meta.env.BASE_API_URL || REACT_APP_API_SERVER_URL;
