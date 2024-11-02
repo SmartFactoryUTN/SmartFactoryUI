a11e34b7a9d7f8241ed21ca4e8184e31035eb61a
Milena <milenalebedin@gmail.com>
2024-11-02 18:59:52 -0300

    clean and moved azulito.svg

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
-import Logo from "../assets/azulito.svg"
+import Logo from "../../public/azulito.svg"
 
 import {AppBar, Button, IconButton, Toolbar, Box, 
     Drawer,
