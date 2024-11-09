17ec97857c7d1150890d3ec75db1113b25c356b3
Tomás <tomasmoreira04@gmail.com>
2024-11-09 03:38:41 -0300

    Mejoradisima la modal de nuevo rollo

--- src/pages/NuevoRollo.tsx
+++ src/pages/NuevoRollo.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState} from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
     Button,
     Dialog,
@@ -6,52 +6,71 @@ import {
     DialogContent,
     DialogTitle,
     FormControl,
-    MenuItem,
-    Select,
     Snackbar,
-    TextField
+    Alert,
+    TextField,
+    InputAdornment,
+    IconButton,
+    createFilterOptions, Typography, CircularProgress, Slide
 } from "@mui/material";
-import {createColor, createRollo, getFabricColors} from "../api/methods.ts"; // Importa la función para crear rollo
-import {FabricColor} from "../utils/types.tsx";
+import { Autocomplete } from "@mui/material";
+import { createColor, createRollo, getFabricColors } from "../api/methods.ts";
+import { FabricColor } from "../utils/types.tsx";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import AddIcon from "@mui/icons-material/Add";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-const NuevoRolloModal = ({open, onClose, onSave}) => {
+const NuevoRolloModal = ({ open, onClose, onSave }) => {
     const [colors, setColors] = useState<FabricColor[]>([]);
     const [selectedColor, setSelectedColor] = useState<FabricColor | null>(null);
     const [newColorName, setNewColorName] = useState("");
     const [rolloName, setRolloName] = useState("");
     const [rolloDescription, setRolloDescription] = useState("");
-    const [successMessage, setSuccessMessage] = useState("");
     const [isSaving, setIsSaving] = useState(false);
     const [isSuccess, setIsSuccess] = useState(false);
-
-    const handleChange = (event: any) => {
-        const selectedId = event.target.value;
-        const selected = colors.find((color) => color.fabricColorId === selectedId);
-        setSelectedColor(selected || null);
-    }
+    const [error, setError] = useState<string | null>(null);
+    const [showSuccessSnackbar, setShowSuccessSnackbar] = useState(false);
+    const [iconType, setIconType] = useState("add");
+    const [isColorValid, setIsColorValid] = useState(false);
+    const [showRolloSuccessSnackbar, setShowRolloSuccessSnackbar] = useState(false);
 
     const handleAddColor = async () => {
         if (newColorName.trim()) {
-            const newColor = {
-                name: newColorName.trim()
-            };
+            if (colors.some(color => color.name.toLowerCase() === newColorName.toLowerCase())) {
+                setError("El color ya existe");
+                return;
+            }
 
-            console.log(newColor);
+            const newColor = { name: newColorName.trim() };
 
             try {
                 const result = await createColor(newColor);
-                const persistedNewColor = result.data
-                console.log(persistedNewColor);
+                const persistedNewColor = result.data;
 
-                setColors((prevColors) => [...prevColors, persistedNewColor]);
-                setNewColorName("");
-                setSelectedColor(persistedNewColor);
+                setIconType("check");
+                setIsColorValid(true);
+
+                setTimeout(() => {
+                    setColors((prevColors) => [...prevColors, persistedNewColor]);
+                    setSelectedColor(persistedNewColor);
+                    setNewColorName("");
+                    setShowSuccessSnackbar(true);
+                    setIconType("add");
+                }, 1500);
             } catch (error) {
-                console.log("Error al agregar el color:", error);
+                setError("Hubo un error al agregar el color. Intente nuevamente.");
             }
+        }
+    };
+
+    const handleColorChange = (_, newValue) => {
+        if (typeof newValue === 'string') {
+            setNewColorName(newValue);
+            setSelectedColor(null);
+            setIsColorValid(false);
+        } else {
+            setSelectedColor(newValue);
+            setNewColorName("");
+            setIsColorValid(!!newValue); // true si es un color existente
         }
     };
 
@@ -65,33 +84,24 @@ const NuevoRolloModal = ({open, onClose, onSave}) => {
         }
     }, [open]);
 
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string | null>(null);
-
     const fetchColors = useCallback(async () => {
-        if (isLoading) return;
-        setIsLoading(true);
         try {
             const result = await getFabricColors();
             if (result.status === 'success') {
                 setColors(result.data);
             } else {
-                console.error('Failed to fetch colors:', result.data);
                 setError('Failed to fetch colors. Please try again.');
             }
         } catch (error) {
-            console.error('Error fetching colors:', error);
-        } finally {
-            setIsLoading(false);
+            setError('Error fetching colors');
         }
-    }, [isLoading]);
+    }, []);
 
     const handleSave = async () => {
         setIsSaving(true);
         setIsSuccess(false);
 
-        if (!rolloName || !selectedColor || !rolloDescription) {
-            // FIXME: mensajitos lindos en vez de alert todo feo
+        if (!rolloName || !rolloDescription || !isColorValid) {
             alert("Por favor, complete todos los campos.");
             setIsSaving(false);
             return;
@@ -101,25 +111,23 @@ const NuevoRolloModal = ({open, onClose, onSave}) => {
             const response = await createRollo({
                 name: rolloName,
                 description: rolloDescription,
-                fabricColorId: selectedColor.fabricColorId
+                fabricColorId: selectedColor ? selectedColor.fabricColorId : ""
             });
 
             if (response.status === 'success') {
                 setIsSuccess(true);
-                setIsSaving(false);
-                setSuccessMessage("¡Rollo guardado exitosamente!");
+                setShowRolloSuccessSnackbar(true);
                 onSave();
                 setTimeout(() => {
                     setIsSuccess(false);
-                    setSuccessMessage("");
                     onClose();
                 }, 3000);
             } else {
-                console.error('Error al guardar el rollo:', response.data.message);
-                setIsSaving(false);
+                setError('Error al guardar el rollo');
             }
         } catch (error) {
-            console.error('Error en la llamada a la API:', error);
+            setError('Error en la llamada a la API');
+        } finally {
             setIsSaving(false);
         }
     };
@@ -129,10 +137,12 @@ const NuevoRolloModal = ({open, onClose, onSave}) => {
         setRolloDescription("");
         setSelectedColor(null);
         setNewColorName("");
+        setIconType("add");
+        setIsColorValid(false);
     };
 
     return (
-        <Dialog open={open} onClose={onClose} maxWidth={false} fullWidth sx={{width: '600px', maxWidth: '90%', margin: 'auto'}}>
+        <Dialog open={open} onClose={onClose} maxWidth={false} fullWidth sx={{ width: '600px', maxWidth: '90%', margin: 'auto' }}>
             <DialogTitle>Nuevo Rollo</DialogTitle>
             <DialogContent>
                 <FormControl fullWidth margin="dense">
@@ -154,57 +164,100 @@ const NuevoRolloModal = ({open, onClose, onSave}) => {
                         value={rolloDescription}
                         onChange={(e) => setRolloDescription(e.target.value)}
                     />
-                    <Select
-                        fullWidth
-                        value={selectedColor?.fabricColorId || ""}
-                        id={"colorSelect"}
-                        onChange={handleChange}
-                        variant={"outlined"}
-                        displayEmpty
-                        sx={{mt: 2}}
-                        renderValue={
-                            selectedColor?.fabricColorId ? undefined : () => "Seleccione color"
-                        }>
-                        <MenuItem value={""} disabled>
-                            Seleccione color
-                        </MenuItem>
-                        {colors.map((color) => (
-                            <MenuItem key={color.fabricColorId} value={color.fabricColorId}>
-                                {color.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <TextField
-                        margin="dense"
-                        label="Agregar nuevo color"
-                        value={newColorName}
-                        onChange={(e) => setNewColorName(e.target.value)}
-                        fullWidth
-                        variant="outlined"
-                        sx={{ mt: 2 }}
+                    <Autocomplete
+                        options={colors}
+                        filterOptions={createFilterOptions({ limit: 5 })}
+                        getOptionLabel={(option) => typeof option === 'string' ? option : option.name}
+                        value={selectedColor}
+                        onChange={handleColorChange}
+                        freeSolo
+                        inputValue={newColorName}
+                        onInputChange={(_, newInputValue) => setNewColorName(newInputValue)}
+                        sx={{
+                            "& .MuiInputBase-root": {
+                                borderColor: newColorName && !colors.some(color => color.name === newColorName) ? "primary.main" : "inherit"
+                            }
+                        }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Seleccione o agregue color"
+                                variant="outlined"
+                                sx={{ mt: 2 }}
+                                InputProps={{
+                                    ...params.InputProps,
+                                    endAdornment: (
+                                        <>
+                                            {params.InputProps.endAdornment}
+                                            {newColorName && !colors.some(color => color.name === newColorName) && (
+                                                <InputAdornment position="end">
+                                                    <IconButton
+                                                        onClick={handleAddColor}
+                                                        edge="end"
+                                                        sx={{
+                                                            transition: "transform 0.3s ease, opacity 0.3s ease",
+                                                            transform: iconType === "check" ? "scale(1.2)" : "scale(1)",
+                                                            opacity: iconType === "add" ? 1 : 0.8,
+                                                        }}
+                                                    >
+                                                        {iconType === "check" ? (
+                                                            <CheckCircleIcon color="success" />
+                                                        ) : (
+                                                            <AddIcon />
+                                                        )}
+                                                    </IconButton>
+                                                </InputAdornment>
+                                            )}
+                                        </>
+                                    ),
+                                }}
+                            />
+                        )}
                     />
-                    <Button
-                        onClick={handleAddColor}
-                        variant="contained"
-                        color="primary"
-                        sx={{ mt: 2 }}
-                    >
-                        Agregar Color
-                    </Button>
+                    <Typography variant="caption" sx={{ mt: 1, color: "text.secondary" }}>
+                        Selecciona un color existente o ingresa uno nuevo para añadirlo.
+                    </Typography>
                 </FormControl>
                 <Snackbar
-                    open={!!successMessage}
+                    open={showSuccessSnackbar}
                     autoHideDuration={3000}
-                    message={successMessage}
-                />
+                    onClose={() => setShowSuccessSnackbar(false)}
+                    TransitionComponent={(props) => <Slide {...props} direction="up" />}
+                >
+                    <Alert onClose={() => setShowSuccessSnackbar(false)} severity="success" sx={{ width: '100%' }}>
+                        Color agregado exitosamente
+                    </Alert>
+                </Snackbar>
+
+                <Snackbar
+                    open={!!error}
+                    autoHideDuration={3000}
+                    onClose={() => setError(null)}
+                    TransitionComponent={(props) => <Slide {...props} direction="up" />}
+                >
+                    <Alert onClose={() => setError(null)} severity="error" sx={{ width: '100%' }}>
+                        {error}
+                    </Alert>
+                </Snackbar>
+
+                <Snackbar
+                    open={showRolloSuccessSnackbar}
+                    autoHideDuration={3000}
+                    onClose={() => setShowRolloSuccessSnackbar(false)}
+                    TransitionComponent={(props) => <Slide {...props} direction="up" />}
+                >
+                    <Alert onClose={() => setShowRolloSuccessSnackbar(false)} severity="success" sx={{ width: '100%' }}>
+                        ¡Rollo creado exitosamente!
+                    </Alert>
+                </Snackbar>
             </DialogContent>
-            <DialogActions sx={{mr: 2, mb: 2}}>
+            <DialogActions sx={{ mr: 2, mb: 2 }}>
                 <Button onClick={onClose}>Cancelar</Button>
                 <Button
-                    startIcon={isSuccess ? <CheckCircleIcon /> : null}
+                    startIcon={isSaving ? <CircularProgress size={20} color="inherit" /> : isSuccess ? <CheckCircleIcon /> : null}
                     variant="contained"
                     onClick={handleSave}
-                    disabled={!!error || !rolloName || !rolloDescription || isSuccess || isLoading || !selectedColor}
+                    disabled={isSaving || !rolloName || !rolloDescription || !isColorValid}
                     sx={{
                         backgroundColor: isSaving ? 'grey' : isSuccess ? 'green' : 'primary.main',
                         color: 'white',
@@ -217,7 +270,7 @@ const NuevoRolloModal = ({open, onClose, onSave}) => {
                         },
                     }}
                 >
-                    {isSaving ? "Guardando..." : isSuccess? "Guardado" : "Guardar"}
+                    {isSaving ? "Guardando..." : isSuccess ? "Guardado" : "Guardar"}
                 </Button>
             </DialogActions>
         </Dialog>
